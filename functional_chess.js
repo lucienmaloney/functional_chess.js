d6f0b7de93680c2bafb7db5ae4fa9a3ad94ccbca
lucienmaloney <lucienmaloney@student.lsmsa.edu>
2017-03-28 16:33:47 -0500

    Added support for a numeric range value alongside the traditional true/false value. The code now supports initial pawn moves.

--- functional_chess.js
+++ functional_chess.js
@@ -14,5 +14,11 @@ const chess_board = FEN.get_board_from_fen( init_fen );
 console.log( chess_board["31"] );
 
 console.log( Move.knight.get_captures( chess_board, 2, 6 ));
+console.log( Move.w_pawn_init.get_moves( chess_board, 3, 2 ));
+console.log( Move.knight.get_moves( chess_board, 3, 3 ));
+console.log( Move.w_pawn_init.get_captures( chess_board, 3, 2 ));
+console.log( Move.w_pawn_init.get_captures( chess_board, 3, 6 ));
+console.log( Move.rook.get_moves( chess_board, 3, 3 ));
+console.log( Move.rook.get_captures( chess_board, 3, 3 ));
 
 })( "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" );
--- move.js
+++ move.js
@@ -21,43 +21,46 @@ function create_move_functions( delta_1, delta_2 = delta_1 ) {
 	return R.concat( move_template( d1, d2 ), move_template( d2, d1 )); 
 }
 
-function get_moves_list_from_func( func, board, x_pos, y_pos, infinite_range ) {
+function get_moves_list_from_func( func, board, x_pos, y_pos, range ) {
 	const new_coords = func( x_pos, y_pos );
 	const new_pos = Helper.xy_to_sqr( new_coords );
+	const valid_sqr = Helper.validate_sqr( new_pos );
 	if( Helper.validate_sqr( new_pos ) && board[new_pos].side === "" ) {
-		if( !infinite_range ) {
-			return new_pos;
+		const new_range = typeof(range) === "number" ? range - 1 : range;
+		if( !new_range ) {
+			return [new_pos];
 		}
-		return R.concat( [new_pos], get_moves_list_from_func( func, board, new_coords[0], new_coords[1], infinite_range ) );
+		return R.concat( [new_pos], get_moves_list_from_func( func, board, new_coords[0], new_coords[1], new_range ) );
 	}
 	return [];
 }
 
-function get_captures_list_from_func( func, board, x_pos, y_pos, infinite_range ) {
+function get_captures_list_from_func( func, board, x_pos, y_pos, range ) {
 	const new_coords = func( x_pos, y_pos );
 	const new_pos = Helper.xy_to_sqr( new_coords );
 	const valid_sqr = Helper.validate_sqr( new_pos );
 	if( valid_sqr ) {
 		const new_pos_side = board[new_pos].side;
-		if( new_pos_side === "" && infinite_range ) {
-			return get_captures_list_from_func( func, board, new_coords[0], new_coords[1], infinite_range );
+		const new_range = typeof(range) === "number" ? range - 1 : range;
+		if( new_pos_side === "" && new_range ) {
+			return get_captures_list_from_func( func, board, new_coords[0], new_coords[1], new_range );
 		} else if( new_pos_side === Helper.get_opposite_color( board.turn )) {
-			return new_pos;
+			return [new_pos];
 		}
 	}
 	return [];
 }
 
-function create_moves( function_list, range_bool ) {
+function create_moves( function_list, range ) {
 	return function( board, x, y ) {
-		const make_moves = f => get_moves_list_from_func( f, board, x, y, range_bool );
+		const make_moves = f => get_moves_list_from_func( f, board, x, y, range );
 		return R.flatten( R.map( make_moves, function_list ));			
 	}
 }
 
-function create_captures( function_list, range_bool ) {
+function create_captures( function_list, range ) {
 	return function( board, x, y ) {
-		const make_captures = f => get_captures_list_from_func( f, board, x, y, range_bool );
+		const make_captures = f => get_captures_list_from_func( f, board, x, y, range );
 		return R.flatten( R.map( make_captures, function_list ));
 	}
 }
@@ -66,12 +69,14 @@ function create_captures( function_list, range_bool ) {
 // Range is whether the piece stops after one step or keeps going until hitting a wall or a piece
 // Capturing_movement is the list of function denoting how the piece captures. 
 // Capturing_movement is the same as movement for all pieces except the pawn
-function Piece( movement, range, capturing_movement = movement ) {
+// Ditto for capturing_range
+function Piece( movement, range, capturing_movement = movement, capturing_range = range ) {
 	this.movement  = movement;
-	this.range     = range;
-	this.capturing = capturing_movement;
-	this.get_moves = create_moves( this.movement, this.range );
-	this.get_captures = create_captures( this.capturing, this.range );
+	this.range     = range;     // Range can be true for infinite range, false for range of 1, or a number for a numeric range value
+	this.capturing       = capturing_movement;
+	this.capturing_range = capturing_range;
+	this.get_moves    = create_moves( this.movement, this.range );
+	this.get_captures = create_captures( this.capturing, this.capturing_range );
 }
 
 module.exports = {
@@ -81,7 +86,10 @@ module.exports = {
 	queen : new Piece( R.concat( create_move_functions( 1 ), create_move_functions( 0, 1 )), true  ),
 	king  : new Piece( R.concat( create_move_functions( 1 ), create_move_functions( 0, 1 )), false ),
 	w_pawn: new Piece( move_template( [0], [ 1] ), false, move_template( [-1, 1], [ 1] )),
-	b_pawn: new Piece( move_template( [0], [-1] ), false, move_template( [-1, 1], [-1] ))
+	b_pawn: new Piece( move_template( [0], [-1] ), false, move_template( [-1, 1], [-1] )),
+	// On a pawn's first move, it can either move one or two steps. First move pawns need different objects:
+	w_pawn_init: new Piece( move_template( [0], [ 1] ), 2, move_template( [-1, 1], [ 1] ), false ),
+	b_pawn_init: new Piece( move_template( [0], [-1] ), 2, move_template( [-1, 1], [-1] ), false )
 }
 
 })();
