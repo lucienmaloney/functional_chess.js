7409452c29ca7cedfd54d7825f9bb7072a1fb544
lucienmaloney <lucienmaloney@student.lsmsa.edu>
2017-03-31 20:42:45 -0500

    Redid log function to be better. Again.

--- game.js
+++ game.js
@@ -4,35 +4,39 @@
 const R     = require('ramda');
 const MoveG = require('./move_generation');
 
-function display_sqr( sqr ) {
-	if( sqr.side ) {
-		if( sqr.side === "w" ) {
-			return "   \x1b[30m\x1b[47m" + sqr.piece.toUpperCase() + "\x1b[0m";
-		} else if( sqr.side === "b" ) {
-			return "   \x1b[37m\x1b[40m" + sqr.piece.toUpperCase() + "\x1b[0m";
+function log_board( board ) {
+	const map_indexed = R.addIndex( R.map );
+	const sqr_array = R.reverse( R.transpose( R.splitEvery( 8, R.values(board.square_list))));
+	const output_1  = "\x1b[41m     \x1b[42m     ".repeat(4) + "\x1b[0m";
+	const output_2  = "\x1b[42m     \x1b[41m     ".repeat(4) + "\x1b[0m";
+
+	const output_square = function( sqr, index, outer_index ) {
+		if( (index + outer_index) % 2 ) {
+			if( sqr.side === ""  ) return "\x1b[41m     \x1b[0m";
+			if( sqr.side === "w" ) return `\x1b[41m  \x1b[37m${ sqr.piece }\x1b[41m  \x1b[0m`;
+			if( sqr.side === "b" ) return `\x1b[41m  \x1b[30m${ sqr.piece }\x1b[41m  \x1b[0m`;
 		} else {
-			throw "There is a square on the board with an invalid side: " + sqr;
+			if( sqr.side === ""  ) return "\x1b[42m     \x1b[0m";
+			if( sqr.side === "w" ) return `\x1b[42m  \x1b[37m${ sqr.piece }\x1b[42m  \x1b[0m`;
+			if( sqr.side === "b" ) return `\x1b[42m  \x1b[30m${ sqr.piece }\x1b[42m  \x1b[0m`;			
 		}
 	}
-	if( (sqr.x + sqr.y) % 2 ) {
-		return "\x1b[32m   *\x1b[0m";
-	} else {
-		return "\x1b[37m   *\x1b[0m";
-	}
-}
 
-function log_board( board ) {
-	const sqr_array = R.reverse( R.transpose( R.splitEvery( 8, R.values(board.square_list))));
-	console.log("_______________________________________");
-	console.log("|                                     |");
-	const display_sublist = function( sub ) {
-		console.log("|                                     |");
-		console.log( "|", R.reduce( R.concat, "", R.map( display_sqr, sub )), "   |" );
+	const output_line = function( row, index ) {
+		if( index % 2 ) {
+			console.log( output_1 );
+			const squares = map_indexed( (val, idx) => output_square( val, idx, index), row );
+			console.log( R.reduce( R.concat, "", squares ));
+			console.log( output_1 );
+		} else {
+			console.log( output_2 );
+			const squares = map_indexed( (val, idx) => output_square( val, idx, index), row );
+			console.log( R.reduce( R.concat, "", squares ));
+			console.log( output_2 );
+		}
 	}
-	R.map( display_sublist, sqr_array );
-	console.log("|                                     |");
-	console.log("_______________________________________");
-	return "\n";
+	map_indexed( output_line, sqr_array );
+	console.log("\n\n");
 }
 
 function play_random( board, moves ) {
